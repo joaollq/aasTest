f40f445bd8c6e54510c2dee303fe145d14b45c3a
João Quitério <joaoquit@microsoft.com>
2021-07-02 08:10:38 +0100

    AddFirewallRule

---
+++ AddFirewallRule.ps1
@@ -0,0 +1,77 @@
+﻿[CmdletBinding()]
+param(
+    [Parameter(ValueFromPipeline = $true)][String] $ResourceName,
+    [Parameter(ValueFromPipeline = $true)][String] $ResourceGroup
+     )
+
+$ExistingFirewallRuleName = "AzureDevOps"
+$PubIPSource = "ipinfo.io/ip"
+                    
+$AServiceServer = Get-AzAnalysisServicesServer -Name $(ResourceName) -ResourceGroupName $(ResourceGroup)
+$FirewallRules = ($AServiceServer).FirewallConfig.FirewallRules
+$FirewallRuleNameList = $FirewallRules.FirewallRuleName
+$powerBi = ($AServiceServer).FirewallConfig.EnablePowerBIService
+                    
+#Getting previous IP from firewall rule, and new public IP
+$currentIP = (Invoke-WebRequest -uri $PubIPSource -UseBasicParsing).content.TrimEnd()
+                    
+#Updating rules if request is coming from new IP address.
+Write-Output "Updating Analysis Service firewall config"
+$ruleNumberIndex = 1
+$Rules = @() -as [System.Collections.Generic.List[Microsoft.Azure.Commands.AnalysisServices.Models.PsAzureAnalysisServicesFirewallRule]]
+                
+#Storing Analysis Service firewall rules
+$FirewallRules | ForEach-Object {
+    $ruleNumberVar = "rule" + "$ruleNumberIndex"
+    #Exception of storage of firewall rule is made for the rule to be updated
+    if (!($_.FirewallRuleName -match "$ExistingFirewallRuleName")) {
+                
+        $start = $_.RangeStart
+        $end = $_.RangeEnd
+        $tempRule = New-AzAnalysisServicesFirewallRule `
+            -FirewallRuleName $_.FirewallRuleName `
+            -RangeStart $start `
+            -RangeEnd $end
+                
+        Set-Variable -Name "$ruleNumberVar" -Value $tempRule
+        $Rules.Add((Get-Variable $ruleNumberVar -ValueOnly))
+        $ruleNumberIndex = $ruleNumberIndex + 1
+    }
+}
+                    
+Write-Output $FirewallRules         #Write all FireWall Rules to Host
+                
+#Add rule for new IP
+$updatedRule = New-AzAnalysisServicesFirewallRule `
+    -FirewallRuleName "$ExistingFirewallRuleName" `
+    -RangeStart $currentIP `
+    -RangeEnd $currentIP
+                    
+$ruleNumberVar = "rule" + "$ruleNumberIndex"
+Set-Variable -Name "$ruleNumberVar" -Value $updatedRule
+$Rules.Add((Get-Variable $ruleNumberVar -ValueOnly))
+                
+#Creating Firewall config object
+if ($powerBi) {
+        $conf = New-AzAnalysisServicesFirewallConfig -EnablePowerBiService -FirewallRule $Rules 
+    }
+else {       
+        $conf = New-AzAnalysisServicesFirewallConfig -FirewallRule $Rules 
+    }
+                    
+#Setting firewall config
+if ([String]::IsNullOrEmpty($AServiceServer.BackupBlobContainerUri)) {
+    $AServiceServer | Set-AzAnalysisServicesServer `
+        -FirewallConfig $conf `
+        -DisableBackup `
+        -Sku $AServiceServer.Sku.Name.TrimEnd()
+}
+else {
+    $AServiceServer | Set-AzAnalysisServicesServer `
+        -FirewallConfig $conf `
+        -BackupBlobContainerUri $AServiceServer.BackupBlobContainerUri `
+        -Sku $AServiceServer.Sku.Name.TrimEnd()
+                    
+}
+Write-Output "Updated firewall rule to include current IP: $currentIP"
+Write-Output "Enable Power Bi Service was set to: $powerBi" 
